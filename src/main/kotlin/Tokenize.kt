b517894818a6e669358694a2856e267c6d6e2427
Richard <richard.knieper001@stud.fh-dortmund.de>
2021-04-24 12:25:42 +0200

    Implemented tokenizer and basic math interpretation

---
+++ src/main/kotlin/Tokenize.kt
@@ -0,0 +1,46 @@
+import java.lang.Double.parseDouble
+import java.lang.NumberFormatException
+
+fun tokenize(input: String) = input.parseToList().parseToTokens()
+
+private fun String.parseToList(): List<String> {
+    return this.trimIndent()
+        .replace("\n", "")
+        .replace("(", " ( ")
+        .replace(")", " ) ")
+        .split(" ")
+        .filter { it.isNotEmpty() }
+}
+
+private fun List<String>.parseToTokens(): List<Any> {
+    val stack = mutableListOf<MutableList<Any>>()
+    stack.add(0, mutableListOf())
+    this.forEach {
+        when (it) {
+            "(" -> stack.add(0, mutableListOf())
+            ")" -> {
+                val l = stack.removeAt(0)
+                stack[0].add(l)
+            }
+            else -> stack[0].add(it.toToken())
+        }
+    }
+    return stack[0]
+}
+
+private fun String.toToken(): Any {
+    return try {
+        parseDouble(this)
+    } catch (e: NumberFormatException) {
+        when(this) {
+
+            // Math operations
+            "+" -> Token.ADD
+            "-" -> Token.SUBTRACT
+            "*" -> Token.MULTIPLY
+            "/" -> Token.DIVIDE
+
+            else -> this
+        }
+    }
+}
