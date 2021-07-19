b162574aadcdbbcfe40b39cc5a5c059c495f169d
Richard <richard.knieper001@stud.fh-dortmund.de>
2021-07-19 12:41:26 +0200

    Added build process for fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,12 +18,23 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+tasks {
+    test {
+        useJUnitPlatform()
+    }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    withType<Jar> {
+        manifest {
+            attributes["Main-Class"] = "MainKt"
+        }
+        from(configurations.compileClasspath.map { config ->
+            config.map { if (it.isDirectory) it else zipTree(it) }
+        })
+    }
 }
 
 application {
